23fe0b104ad6d3a159619be432d0d7b81ce95a98
andereyaa <andrewmamawi@gmail.com>
2024-05-04 15:35:30 +0300

    completed exercise

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,22 @@
 // TODO: Write your function in this file
 
+function fizzbuzz(lower, upper){
+    let answer = []
+    let value;
+    for(let i=lower; i<=upper;i++){
+        value = i
+        if(i%3==0){
+            value = 'Fizz'
+        }
+        if(i%5==0){
+            value = 'Buzz'
+        }
+        if(i%3==0&&i%5==0){
+            value = 'FizzBuzz'
+        }
+        answer[i-lower] = value
+    }
+    return answer
+}
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzbuzz
